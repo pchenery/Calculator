72b4d77e1736c19ae4055e91c4c18ad520e6242a
Peter Chenery <peter.chenery@anthonynolan.org>
2017-10-09 17:02:30 +0100

    Added all Operators

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -10,14 +10,35 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to the Calculator!");
+            Console.WriteLine("========================== ");
+            Console.WriteLine("Please enter the Operator: ");
+            string Operator = Console.ReadLine();
+
             Console.WriteLine("Enter the first number: ");
             string myFirstNumber = Console.ReadLine();
 
             Console.WriteLine("Enter the second number: ");
             string mySecondNumber = Console.ReadLine();
 
-            int myMultiple = int.Parse(myFirstNumber) * int.Parse(mySecondNumber);
-            Console.WriteLine($"Multiple is: {myMultiple}");
+            float output = 0.000f;
+            switch (Operator)
+            {
+                case "+":
+                    output = int.Parse(myFirstNumber) + int.Parse(mySecondNumber);
+                    break;
+                case "-":
+                    output = int.Parse(myFirstNumber) - int.Parse(mySecondNumber);
+                    break;
+                case "x":
+                    output = int.Parse(myFirstNumber) * int.Parse(mySecondNumber);
+                    break;
+                case "/":
+                    output = float.Parse(myFirstNumber) / float.Parse(mySecondNumber);
+                    break;
+            }
+
+            Console.WriteLine($"Result is: {output}");
             Console.ReadLine();
         }
     }

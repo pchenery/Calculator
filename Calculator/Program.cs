22bc775cca1556707730d229b496659d26972630
Peter Chenery <peter.chenery@anthonynolan.org>
2017-10-15 15:13:44 +0100

    More than one operator

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -15,27 +15,37 @@ namespace Calculator
             Console.WriteLine("Please enter the Operator: ");
             string Operator = Console.ReadLine();
 
-            Console.WriteLine("Enter the first number: ");
-            string myFirstNumber = Console.ReadLine();
+            Console.WriteLine("How many numbers do you want to " + Operator + "? ");
+            int totalNumber = int.Parse(Console.ReadLine());
+            int[] inputs = new int[totalNumber];
 
-            Console.WriteLine("Enter the second number: ");
-            string mySecondNumber = Console.ReadLine();
-
-            float output = 0.000f;
-            switch (Operator)
+            for (int i = 0; i < totalNumber; i++)
             {
-                case "+":
-                    output = int.Parse(myFirstNumber) + int.Parse(mySecondNumber);
-                    break;
-                case "-":
-                    output = int.Parse(myFirstNumber) - int.Parse(mySecondNumber);
-                    break;
-                case "x":
-                    output = int.Parse(myFirstNumber) * int.Parse(mySecondNumber);
-                    break;
-                case "/":
-                    output = float.Parse(myFirstNumber) / float.Parse(mySecondNumber);
-                    break;
+
+                Console.WriteLine("Please enter Number " + (i + 1));
+                inputs[i] = int.Parse(Console.ReadLine());
+            }
+
+            float output = inputs[0];
+
+            for (int index = 1; index < totalNumber; index++)
+            {
+                if (Operator == "*")
+                {
+                    output = output * inputs[index];
+                }
+                else if (Operator == "/")
+                {
+                    output = output / inputs[index];
+                }
+                else if (Operator == "+")
+                {
+                    output = output + inputs[index];
+                }
+                else if (Operator == "-")
+                {
+                    output = output - inputs[index];
+                }
             }
 
             Console.WriteLine($"Result is: {output}");
@@ -43,3 +53,4 @@ namespace Calculator
         }
     }
 }
+
